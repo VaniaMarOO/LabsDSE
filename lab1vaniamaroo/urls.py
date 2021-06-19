668e87d9332c56f33ad93d5c7645b399180706e7
VaniaMarOO <vm.olivoso@alum.up.edu.pe>
2021-06-18 22:38:33 -0500

    heroku ready commit 1

--- lab1vaniamaroo/urls.py
+++ lab1vaniamaroo/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path("", include("main.urls")),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
